85d2c8847fa32b81f54e7da4032730ee02ebdc4e
jbastoscor <jbastoscor@gmail.com>
2025-02-14 20:28:30 -0300

    alterado!
    alterado!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function appendValue(value) {
   const result = document.getElementById('result');
-  if (result.value === "0" && value !== ".") {
+  if (result.value === '0' && value !== '.') {
     result.value = value;
   } else {
     result.value += value;
@@ -8,14 +8,33 @@ function appendValue(value) {
 }
 
 function clearResult() {
-  document.getElementById('result').value = "0";
+  document.getElementById('result').value = '0';
 }
 
 function calculateResult() {
   const result = document.getElementById('result');
   try {
-    result.value = eval(result.value.replace(/x/g, "*"));
+    result.value = eval(result.value);
   } catch (e) {
-    result.value = "Error";
+    result.value = 'Error';
   }
-  }
+}
+
+function handleInput() {
+  const input = document.getElementById('result');
+  input.addEventListener('input', (e) => {
+    e.target.value = e.target.value.replace(/[^0-9+\-*/]/g, '');
+  });
+}
+handleInput();
+
+function handleEnter() {
+  window.addEventListener('keydown', (e) => {
+    console.log(e);
+    if (e.key === 'Enter') {
+      calculateResult();
+    }
+  });
+}
+
+handleEnter();
